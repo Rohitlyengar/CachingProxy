4ddd5cda2bf9106bceb1b49adcfdfc0226fabe46
Rohit Iyengar <rohitiyengar8@gmail.com>
2025-06-15 17:12:17 +0530

    Setup basic caching

--- index.js
+++ index.js
@@ -10,8 +10,26 @@ const cache = new NodeCache({stdTTL: process.env.TTL ||  60});
 const PORT = process.env.PORT || 3000;
 
 app.get('/', async (req, res) => {
-    res.send('Hello World');
+    const cacheKey = 'Key';
+    const cachedData = cache.get(cacheKey);
+
+    if(cachedData){
+        res.send(cachedData);
+        return;
+    }
+    try {
+        const freshData =  await fetchData(req.query.url);
+        cache.set(cacheKey, freshData);
+        res.send(freshData);
+    }
+    catch(error){
+        res.status(500).send('Error fetching data')
+    }
 })
+
+fetchData = async(source) => {
+    return await(await fetch(source)).text();
+}
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
